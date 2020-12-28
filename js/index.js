b70f1f373ccfc9eb6dbc59f1bb3fd46f925b3ee2
chepite <julesvictor@telenet.be>
2020-12-28 23:36:37 +0100

    firebase init

--- js/index.js
+++ js/index.js
@@ -1,4 +1,23 @@
-const data = [{ date: `2020-12-28`, watervalue: 6 }];
+//database shit
+
+// Set the configuration for your app
+  // TODO: Replace with your project's config object
+  var config = {
+    apiKey: "AIzaSyCj1mCRNiAWSbfMTiqLdgcYyZJJt8xPS3Y",
+    authDomain: "planner-e36e7.firebaseapp.com",
+    databaseURL: "https://planner-e36e7-default-rtdb.europe-west1.firebasedatabase.app",
+    storageBucket: "planner-e36e7.appspot.com"
+  };
+  firebase.initializeApp(config);
+
+  // Get a reference to the database service
+  var database = firebase.database();
+
+
+//end database shit
+
+
+const data = [];
 /*structure = {date: datum, 
 water: int} 
 */
@@ -28,12 +47,16 @@ function waterplus(e) {
     const $watervalue = document.querySelector(`#waterTeller`);
     let value = parseInt($watervalue.textContent);
     value++;
-    for (i = 0; i < data.length; i++) {
+    //add value to database
+    /*for (i = 0; i < data.length; i++) {
         if (data[i].date == datum) {
             data[i].watervalue = value;
             console.log(data[i]);
         }
-    }
+    }*/
+
+    
+    //display new data on website
     $watervalue.textContent = value.toString();
 }
 function loadData(e) {
@@ -41,12 +64,18 @@ function loadData(e) {
     const $date = document.querySelector(`.datePicker`);
     let datum = $date.value.toString();
     const $watervalue = document.querySelector(`#waterTeller`);
-    for (i = 0; i < data.length; i++) {
+    //add value to database
+    /*for (i = 0; i < data.length; i++) {
         if (data[i].date == datum) {
             $watervalue.textContent = data[i].watervalue.toString();
         }
-    }
+        else{
+            $watervalue.textContent = `0`;
+        }
+    }*/
 
+
+    //display new data on website
 
 }
 
