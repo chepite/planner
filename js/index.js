bffb30f0ceb756a2486b6a1150cd92117e36cd9f
chepite <julesvictor@telenet.be>
2020-12-29 11:50:11 +0100

    load initial werkende

--- js/index.js
+++ js/index.js
@@ -82,6 +82,7 @@ function loadData(e) {
     //date format: 2020-12-29
     //gets amount of water
     const $locationWater = document.querySelector(`#waterTeller`);
+    $locationWater.innerHTML="";
     let amount;
     let amountURL = firebase.database().ref(`/Days/${datum}/waterAmount`);
     amountURL.on(`value`, (snapshot) => {
@@ -101,6 +102,7 @@ function loadData(e) {
     });
     //fetch ontbijtdata
     const $locationOverviewOntbijt = document.querySelector(`.overviewOntbijt`);
+    $locationOverviewOntbijt.innerHTML="";
     let overviewOntbijtURL = firebase.database().ref(`/Meals/${datum}/ont`);
     overviewOntbijtURL.on(`value`, (snapshot) => {
         const data = snapshot.val();
@@ -124,6 +126,7 @@ function loadData(e) {
         }
     });
     const $locationOverviewTs1 = document.querySelector(`.overviewTs1`);
+    $locationOverviewTs1.innerHTML="";
     let overviewTs1URL = firebase.database().ref(`/Meals/${datum}/ts1`);
     overviewTs1URL.on(`value`, (snapshot) => {
         const data = snapshot.val();
@@ -146,7 +149,102 @@ function loadData(e) {
             });
         }
     });
-
+    const $locationOverviewMid = document.querySelector(`.overviewMid`);
+    $locationOverviewMid.innerHTML="";
+    let overviewMidURL = firebase.database().ref(`/Meals/${datum}/mid`);
+    overviewMidURL.on(`value`, (snapshot) => {
+        const data = snapshot.val();
+        //als nog niet bestaat maak nieuw record aan met water 0
+        if (data == null) {
+            var updates = {};
+            updates[`/Meals/${datum}/mid`] = ``;
+            return firebase.database().ref().update(updates);
+        }
+        else {
+            //als record al bestaat pass de data gewoon
+            let dataArray = data.toString().split(`,`);
+            console.log(dataArray);
+            dataArray.forEach(element => {
+                const $li = document.createElement(`li`);
+                const $p = document.createElement(`p`);
+                $p.textContent = element;
+                $li.appendChild($p);
+                $locationOverviewMid.appendChild($li);
+            });
+        }
+    });
+    const $locationOverviewTs2 = document.querySelector(`.overviewTs2`);
+    $locationOverviewTs2.innerHTML = "";
+    let overviewTs2URL = firebase.database().ref(`/Meals/${datum}/ts2`);
+    overviewTs2URL.on(`value`, (snapshot) => {
+        const data = snapshot.val();
+        //als nog niet bestaat maak nieuw record aan met water 0
+        if (data == null) {
+            var updates = {};
+            updates[`/Meals/${datum}/ts2`] = ``;
+            return firebase.database().ref().update(updates);
+        }
+        else {
+            //als record al bestaat pass de data gewoon
+            let dataArray = data.toString().split(`,`);
+            console.log(dataArray);
+            dataArray.forEach(element => {
+                const $li = document.createElement(`li`);
+                const $p = document.createElement(`p`);
+                $p.textContent = element;
+                $li.appendChild($p);
+                $locationOverviewTs2.appendChild($li);
+            });
+        }
+    });
+    const $locationOverviewAvo = document.querySelector(`.overviewAvo`);
+    $locationOverviewAvo.innerHTML = "";
+    let overviewAvoURL = firebase.database().ref(`/Meals/${datum}/avo`);
+    overviewAvoURL.on(`value`, (snapshot) => {
+        const data = snapshot.val();
+        //als nog niet bestaat maak nieuw record aan met water 0
+        if (data == null) {
+            var updates = {};
+            updates[`/Meals/${datum}/avo`] = ``;
+            return firebase.database().ref().update(updates);
+        }
+        else {
+            //als record al bestaat pass de data gewoon
+            let dataArray = data.toString().split(`,`);
+            console.log(dataArray);
+            dataArray.forEach(element => {
+                const $li = document.createElement(`li`);
+                const $p = document.createElement(`p`);
+                $p.textContent = element;
+                $li.appendChild($p);
+                $locationOverviewAvo.appendChild($li);
+            });
+        }
+    });3
+    const $locationOverviewTs3 = document.querySelector(`.overviewTs3`);
+    $locationOverviewTs3.innerHTML = "";
+    let overviewTs3URL = firebase.database().ref(`/Meals/${datum}/ts3`);
+    overviewTs3URL.on(`value`, (snapshot) => {
+        const data = snapshot.val();
+        //als nog niet bestaat maak nieuw record aan met water 0
+        if (data == null) {
+            var updates = {};
+            updates[`/Meals/${datum}/ts3`] = ``;
+            return firebase.database().ref().update(updates);
+        }
+        else {
+            //als record al bestaat pass de data gewoon
+            let dataArray = data.toString().split(`,`);
+            console.log(dataArray);
+            dataArray.forEach(element => {
+                const $li = document.createElement(`li`);
+                const $p = document.createElement(`p`);
+                $p.textContent = element;
+                $li.appendChild($p);
+                $locationOverviewTs3.appendChild($li);
+            });
+        }
+    });
 
 }
 
