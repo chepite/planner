e55e77d82b300105791b4e59ae1a40da835ad586
chepite <julesvictor@telenet.be>
2020-12-29 01:48:13 +0100

    water teller werkt

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 //database shit
 
 // Set the configuration for your app
-  // TODO: Replace with your project's config object
-  var config = {
+// TODO: Replace with your project's config object
+var config = {
     apiKey: "AIzaSyCj1mCRNiAWSbfMTiqLdgcYyZJJt8xPS3Y",
     authDomain: "planner-e36e7.firebaseapp.com",
     databaseURL: "https://planner-e36e7-default-rtdb.europe-west1.firebasedatabase.app/",
     storageBucket: "planner-e36e7.appspot.com"
-  };
+};
 
-  // Get a reference to the database service
-  var database = firebase.database();
+// Get a reference to the database service
+var database = firebase.database();
 
 
 //end database shit
@@ -24,71 +24,70 @@ function watermin(e) {
     e.preventDefault();
     const $date = document.querySelector(`.datePicker`);
     let datum = $date.value.toString();
-    const $watervalue = document.querySelector(`#waterTeller`);
-    let value = parseInt($watervalue.textContent);
-    if (value > 0) {
-        value--;
-        //  data.find(data.date === datum);
-        for (i = 0; i < data.length; i++) {
-            if (data[i].date == datum) {
-                data[i].watervalue = value;
-                console.log(data[i]);
-            }
-        }
-        $watervalue.textContent = value.toString();
-    }
+    let amount;
+    let amountURL = firebase.database().ref(`/Days/${datum}/waterAmount`); /*later testday veranderen met datum uit datepicker*/
+    amountURL.on(`value`, (snapshot) => {
+        const data = snapshot.val();
+        amount = data;
+        console.log(amount);
+        $location.textContent = amount;
+    })
+    amount--;
+    var updates = {};
+    updates[`/Days/${datum}/waterAmount`] = amount;
+    return firebase.database().ref().update(updates);
 
 }
 function waterplus(e) {
     e.preventDefault();
     const $date = document.querySelector(`.datePicker`);
     let datum = $date.value.toString();
-    const $watervalue = document.querySelector(`#waterTeller`);
-    let value = parseInt($watervalue.textContent);
-    value++;
-    //add value to database
-    /*for (i = 0; i < data.length; i++) {
-        if (data[i].date == datum) {
-            data[i].watervalue = value;
-            console.log(data[i]);
-        }
-    }*/
 
-
-    //display new data on website
-    $watervalue.textContent = value.toString();
+    let amount;
+    let amountURL = firebase.database().ref(`/Days/${datum}/waterAmount`); /*later testday veranderen met datum uit datepicker*/
+    amountURL.on(`value`, (snapshot) => {
+        const data = snapshot.val();
+        amount = data;
+        console.log(amount);
+        $location.textContent = amount;
+    })
+    amount++
+    var updates = {};
+    updates[`/Days/${datum}/waterAmount`] = amount;
+    return firebase.database().ref().update(updates);
 }
+/*firebase.database().ref(`/Days/${datum}/waterAmount`).set({
+    waterAmount: amount
+});*/
+
+
 function loadData(e) {
     e.preventDefault();
     const $date = document.querySelector(`.datePicker`);
     let datum = $date.value.toString();
-    const $p = document.createElement(`p`);
-    const $location= document.querySelector(`.test`);
-    
-    let actualData;
-    let dataURL= firebase.database().ref('/Days/' + `testday` + `/Date`);
-   dataURL.on(`value`, (snapshot)=>{
-       const data= snapshot.val().toString();
-        actualData = data;
-        console.log(actualData);
-   })
-
-   
-
-    
-    $p.textContent = actualData;
-    $location.appendChild($p);
+    //date format: 2020-12-29
+    const $location = document.querySelector(`#waterTeller`);
+    //gets amount of water
+    let amount;
+    let amountURL = firebase.database().ref(`/Days/${datum}/waterAmount`); /*later testday veranderen met datum uit datepicker*/
+    amountURL.on(`value`, (snapshot) => {
+        const data = snapshot.val().toString();
+        amount = data;
+        console.log(amount);
+        $location.textContent = amount.toString();
+    })
     //display new data on website
-
 }
 
 const init = () => {
     //load
-    
-    
+
+
     //buttons shit
+    const $loadbutton = document.querySelector(`#load`);
+    $loadbutton.addEventListener(`click`, loadData);
     const $minbutton = document.querySelector(`#min`);
-    $minbutton.addEventListener(`click`, loadData) /*waterMin was og*/;
+    $minbutton.addEventListener(`click`, watermin) /*waterMin was og*/;
     const $plusbutton = document.querySelector(`#plus`);
     $plusbutton.addEventListener(`click`, waterplus);
 }
